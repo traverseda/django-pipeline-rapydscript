04947abe264e1f1901400ae09be1d02c47bcb7ce
Alex Davies <traverse.da@gmail.com>
2015-03-07 00:13:18 -0400

    First commit.

---
+++ pipeline_rapydscript/conf.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf import settings as _settings
+from pipeline.conf import DEFAULTS, PipelineSettings
+
+DEFAULTS=DEFAULTS.update(
+  {
+    'PIPELINE_RAPYD_SCRIPT_BINARY': '/usr/bin/env coffee',
+    'PIPELINE_RAPYD_SCRIPT_ARGUMENTS': '',
+  }
+)
+
+settings = PipelineSettings(_settings)
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='django-pipeline-rapydscript',
+      version='1.0',
+      description='Use rapydScript with django-pipeline',
+      author='Alex Davies',
+      author_email='traverse.da@gmail.com',
+#      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['pipeline_rapydscript',],
+     )
